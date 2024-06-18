bf59117185dfad427e6b13aa7694adf55647cc3e
Camila <142915203+CamilaNefertite@users.noreply.github.com>
2024-06-18 14:53:05 -0300

    Add files via upload

---
+++ encapsulamento/Carro.java
@@ -0,0 +1,30 @@
+package atividadeAvaliativa2;
+public class Carro {
+	
+	private String modelo;
+	private String fabri;
+	private String anoFabri;
+																																					
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	
+	
+	public void setFabri(String fabri) {
+		this.fabri = fabri;
+	}
+	public String getFabri() {
+		return fabri;
+	}
+	
+	
+	public void setAnoFabri(String anoFabri) {
+		this.anoFabri = anoFabri ;
+	}
+	public String getAnoFabri() {
+		return anoFabri;
+	}										
+}
