9d3c35017ec9b821deca91c11fc348f6abf45bf3
Camila <142915203+CamilaNefertite@users.noreply.github.com>
2024-06-18 21:21:17 -0300

    Add files via upload

--- oop/Aplicacao.java
+++ oop/Aplicacao.java
@@ -1,42 +1,34 @@
-package atividadeAvaliativa1;
+package poo;
 import java.util.Scanner;
+
 public class Aplicacao {
-	public static void main(String[] args) {
-		Scanner in = new Scanner (System.in);
-
-		
-		System.out.println("Qual operação deseja fazer?\n1 para Fatorial | 2 para Tabuada | 3 Área do Triângulo");
-		int opcao = in.nextInt();
-		
-		if(opcao == 1) {
-			MetodosApp maFa = new MetodosApp();
-			
-			System.out.println("Qual número você deseja fatorar?");
-			int numeroFatorial = in.nextInt();
-			
-			System.out.println("\nO fatorial de " +numeroFatorial+ " é de "+maFa.fatorial(numeroFatorial));
-			
-		}else if(opcao == 2) {
-			MetodosApp maTa = new MetodosApp();
-			
-			maTa.tabuada();
-			
-		}else if(opcao==3) {
-			MetodosApp maTr = new MetodosApp();
-			
-			System.out.println("Digite a base do triângulo:");
-			double b = in.nextDouble();
-			
-			System.out.println("Digite a altura do mesmo:");
-			double h = in.nextDouble();
-			
-
-			maTr.triangulo(b, h);
-		}else {
-			System.out.println("Escolha inválida. Escolha novamente!");
-		}
-		
-		
-	
-	}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Digite o 1⁰ numero");
+        int primeiroN = in.nextInt();
+        
+        System.out.println("Digite o 2⁰ numero");
+        int segundoN = in.nextInt();
+        
+        System.out.println("Digite o 3⁰ numero");
+        int terceiroN = in.nextInt();
+        
+        System.out.println("Digite o 4⁰ numero");
+        int quartoN = in.nextInt();
+        
+        Matematica matematica = new Matematica();
+        
+        // Usando a sobrecarga do método somar
+        int resultado2 = matematica.somar(primeiroN, segundoN);     
+        System.out.println("Resultado da soma de 2 números: " + resultado2);
+        
+        int resultado3 = matematica.somar(primeiroN, segundoN, terceiroN);     
+        System.out.println("Resultado da soma de 3 números: " + resultado3);
+        
+        int resultado4 = matematica.somar(primeiroN, segundoN, terceiroN, quartoN);     
+        System.out.println("Resultado da soma de 4 números: " + resultado4);
+        
+        in.close();
+    }
 }
---
+++ oop/Matematica.java
@@ -0,0 +1,18 @@
+package poo;
+
+public class Matematica {
+    public int somar(int primeiroN, int segundoN) {
+        return primeiroN + segundoN;
+    }
+    
+    public int somar(int primeiroN, int segundoN, int terceiroN) {
+        return primeiroN + segundoN + terceiroN;
+    }
+    
+    public int somar(int primeiroN, int segundoN, int terceiroN, int quartoN) {
+        return primeiroN + segundoN + terceiroN + quartoN;
+    }
+    
+    
+    
+}
